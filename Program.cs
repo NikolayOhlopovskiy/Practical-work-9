1d59c5d1b93c7beed1d0d44946d7287fb2e9601d
NikolayOhlopovskiy <nik.okhlopovskiy.17@gmail.com>
2024-11-05 14:35:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9__10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1.Определить, имеются ли в одномерном массиве случайных чисел элементы,
+            //значения которых кратны 11. Вывести такие значения.
+            //const int n = 12;
+            //int[] omas = new int[n];
+            //Random rnd = new Random();
+            //for(int i=0;i<omas.Length;i++)
+            //{
+            //   omas[i]=rnd.Next(0,99);
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+
+            //foreach(int element in omas)
+            //{
+
+            //    Console.WriteLine(element);
+            //}
+
+            //Задача 2
+            //int[] omas = { 5, -4, 8, 7, -9 };
+            //int count=0, value = 0;
+            //for(int i =0; i<omas.Length;i++)
+            //{
+            //    if(omas[i]<0)
+            //    {
+            //        value += omas[i];
+            //        count++;
+            //    }
+            //}
+            //Console.Write($"Сумма:{value}\n Количество:{count}");
+
+
+            //Задача 3
+            //int[] omas = new int[5];
+            //Console.Write("Заполним одномерный массив");
+            //for (int i= 0;i<omas.Length;i++)
+            //{
+            //    Console.WriteLine($"omas=[{i}]");
+            //    omas[i] = int.Parse(Console.ReadLine());
+                
+            //}
+            
+            //omas.Reverse();
+
+
+            Console.ReadKey();
+        }
+    }
+}
